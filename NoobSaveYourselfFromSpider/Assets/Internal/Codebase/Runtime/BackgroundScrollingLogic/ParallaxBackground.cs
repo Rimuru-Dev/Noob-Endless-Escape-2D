55d4a6f037175b21e18dd4229158d826dc2ae7e5
RimuruDev <Rimuru.Dev@gmail.com>
2023-07-13 16:09:55 +0400

    Save

--- NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/BackgroundScrollingLogic/ParallaxBackground.cs
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/BackgroundScrollingLogic/ParallaxBackground.cs
@@ -13,119 +13,85 @@ namespace Internal.Codebase.Runtime.BackgroundScrollingLogic
     public sealed class ParallaxBackground : MonoBehaviour
     {
         public Transform[] backgrounds;
-        public float parallaxScale;
-        public float parallaxSpeed;
-        [SerializeField] private Camera mainCamera;
+        public float parallaxScale = 0;
+        public float parallaxSpeed = 1;
 
-        private float[] backgroundInitialPositions;
+        private float[] backgroundWidths;
+        private float rightmostBackgroundX;
 
         private void Start()
         {
-            mainCamera ??= Camera.main;
-            backgroundInitialPositions = new float[backgrounds.Length];
+            backgroundWidths = new float[backgrounds.Length];
 
             for (var i = 0; i < backgrounds.Length; i++)
             {
-                backgroundInitialPositions[i] = backgrounds[i].position.x;
+                backgroundWidths[i] = GetSpriteWidth(backgrounds[i]);
             }
-        }
 
-        // private void Update()
-        // {
-        //     for (var i = 0; i < backgrounds.Length; i++)
-        //     {
-        //         var backgroundTargetPositionX =
-        //             backgrounds[i].position.x + parallaxSpeed * (i * parallaxScale + 1) * Time.deltaTime;
-        //
-        //         var backgroundTargetPosition = new Vector3(backgroundTargetPositionX, backgrounds[i].position.y,
-        //             backgrounds[i].position.z);
-        //
-        //         backgrounds[i].position = backgroundTargetPosition;
-        //
-        //         // Продление фона из префабов
-        //         if (backgrounds[i].position.x < backgroundInitialPositions[i])
-        //         {
-        //             var rightmostBackgroundX = GetRightmostBackgroundX();
-        //
-        //             backgrounds[i].position = new Vector3(rightmostBackgroundX + backgroundInitialPositions[i],
-        //                 backgrounds[i].position.y, backgrounds[i].position.z);
-        //         }
-        //     }
-        // }
+            rightmostBackgroundX = GetRightmostBackgroundX();
+        }
 
         private void Update()
         {
-            float screenWidthInWorldPoint = CalculateScreenWidthInWorldPoint();
-
-            Debug.Log(screenWidthInWorldPoint);
-
-            for (int i = 0; i < backgrounds.Length; i++)
+            for (var i = 0; i < backgrounds.Length; i++)
             {
                 var backgroundTargetPositionX =
                     backgrounds[i].position.x + parallaxSpeed * (i * parallaxScale + 1) * Time.deltaTime;
-
                 var backgroundTargetPosition = new Vector3(backgroundTargetPositionX, backgrounds[i].position.y,
                     backgrounds[i].position.z);
-
                 backgrounds[i].position = backgroundTargetPosition;
 
-                // Проверяем, если фон вышел за пределы экрана справа
-                if (backgrounds[i].position.x < screenWidthInWorldPoint)
+                if (backgrounds[i].position.x + backgroundWidths[i] < rightmostBackgroundX)
                 {
-                    Debug.Log("backgrounds[i].position.x < screenWidthInWorldPoint");
-
-                    var leftmostBackgroundX = GetLeftmostBackgroundX();
-                    var backgroundWidth = backgrounds[i].GetComponent<SpriteRenderer>().bounds.size.x;
-                    var newBackgroundX = leftmostBackgroundX - backgroundWidth;
-
-                    backgrounds[i].position =
-                        new Vector3(newBackgroundX, backgrounds[i].position.y, backgrounds[i].position.z);
+                    MoveBackgroundToEnd(i);
                 }
             }
+
+            rightmostBackgroundX = GetRightmostBackgroundX();
         }
 
-        private float CalculateScreenWidthInWorldPoint()
+        private float GetSpriteWidth(Transform spriteTransform)
         {
-            var screenPoint = new Vector3(Screen.width, 0, mainCamera.transform.position.z);
-            var screenWidthInWorldPoint = mainCamera.ScreenToWorldPoint(screenPoint).x;
-            return screenWidthInWorldPoint;
+            var spriteRenderer = spriteTransform.GetComponent<SpriteRenderer>();
+            var spriteBounds = spriteRenderer.bounds;
+            return spriteBounds.size.x;
         }
 
-        private float GetLeftmostBackgroundX()
-        {
-            var leftmostX = float.MaxValue;
-
-            for (int i = 0; i < backgrounds.Length; i++)
-            {
-                if (backgrounds[i].position.x < leftmostX)
-                    leftmostX = backgrounds[i].position.x;
-            }
-
-            return leftmostX;
-        }
-
-        /// <summary>
-        /// This method returns the `x` coordinate of the rightmost background from the 'backgrounds' array.
-        /// </summary>
-        /// <code>
-        ///     private float GetRightmostBackgroundX() => 
-        ///         backgrounds
-        ///         .Select(background => background.position.x)
-        ///         .Prepend(float.MinValue)
-        ///        .Max();
-        /// </code>
-        /// <returns>rightmostX</returns>
         private float GetRightmostBackgroundX()
         {
             var rightmostX = float.MinValue;
 
             for (var i = 0; i < backgrounds.Length; i++)
             {
-                if (backgrounds[i].position.x > rightmostX)
-                    rightmostX = backgrounds[i].position.x;
+                var backgroundX = backgrounds[i].position.x + backgroundWidths[i] / 2;
+                if (backgroundX > rightmostX)
+                    rightmostX = backgroundX;
             }
 
             return rightmostX;
+        }
+
+        private void MoveBackgroundToEnd(int backgroundIndex)
+        {
+            var leftmostBackgroundX = GetLeftmostBackgroundX();
+            var newBackgroundX = rightmostBackgroundX + backgroundWidths[backgroundIndex];
+            var backgroundPosition = backgrounds[backgroundIndex].position;
+            backgroundPosition.x = leftmostBackgroundX + (newBackgroundX - rightmostBackgroundX) / 2;
+            backgrounds[backgroundIndex].position = backgroundPosition;
+        }
+
+        private float GetLeftmostBackgroundX()
+        {
+            var leftmostX = float.MaxValue;
+
+            for (var i = 0; i < backgrounds.Length; i++)
+            {
+                var backgroundX = backgrounds[i].position.x - backgroundWidths[i] / 2;
+                if (backgroundX < leftmostX)
+                    leftmostX = backgroundX;
+            }
+
+            return leftmostX;
         }
     }
 }
