c13571a5c71ce9f7e81db214eef197cc1ce07377
RimuruDev <Rimuru.Dev@gmail.com>
2023-07-14 12:22:24 +0400

    Save

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/ProceduralLevelGerenationLogic/LevelGenerator.cs
@@ -0,0 +1,130 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: rimuru.dev@gmail.com
+//
+// **************************************************************** //
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Internal.Codebase.Runtime.ProceduralLevelGerenationLogic
+{
+    public sealed class LevelGenerator : MonoBehaviour
+    {
+        public Transform player;
+        public Tilemap tilemap;
+        public GameObject[] prefabTiles;
+        public float spawnDistance = 10f;
+        public int maxTiles = 5;
+
+        private List<GameObject> spawnedTiles = new List<GameObject>();
+        private float totalWidth = 0f;
+        private float tileWidth = 0f;
+
+        private void Start()
+        {
+            tileWidth = GetTileWidth(prefabTiles[0]);
+            GenerateInitialTiles();
+        }
+
+        private void Update()
+        {
+            MoveLevel();
+            CheckTiles();
+        }
+
+        private void GenerateInitialTiles()
+        {
+            float xPosition = 0f;
+            for (int i = 0; i < maxTiles; i++)
+            {
+                GameObject tile = SpawnRandomTile();
+                tile.transform.position = new Vector3(xPosition, 0f, 0f);
+                spawnedTiles.Add(tile);
+                xPosition += tileWidth;
+                totalWidth += tileWidth;
+
+                if (i > 0)
+                {
+                    AttachTileToPrevious(tile, spawnedTiles[i - 1]);
+                }
+            }
+        }
+
+        private void MoveLevel()
+        {
+            float moveSpeed = 2f; // Устанавливайте свою скорость движения уровня
+            float newXPosition = transform.position.x - moveSpeed * Time.deltaTime;
+            transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+        }
+
+        private void CheckTiles()
+        {
+            float despawnX = transform.position.x - (tileWidth * 2f);
+
+            foreach (GameObject tile in spawnedTiles)
+            {
+                float tileX = tile.transform.position.x;
+                if (tileX < despawnX)
+                {
+                    spawnedTiles.Remove(tile);
+                    Destroy(tile);
+                    SpawnNextTile(despawnX);
+                    break;
+                }
+            }
+        }
+
+        private void SpawnNextTile(float despawnX)
+        {
+            GameObject tile = SpawnRandomTile();
+            tile.transform.position = new Vector3(despawnX + tileWidth, 0f, 0f);
+            spawnedTiles.Add(tile);
+            totalWidth += tileWidth;
+
+            AttachTileToPrevious(tile, spawnedTiles[spawnedTiles.Count - 2]);
+        }
+
+        private void AttachTileToPrevious(GameObject currentTile, GameObject previousTile)
+        {
+            Bounds currentBounds = GetTileBounds(currentTile);
+            Bounds previousBounds = GetTileBounds(previousTile);
+
+            float offsetX = currentBounds.center.x - currentBounds.min.x + previousBounds.center.x -
+                            previousBounds.max.x;
+            float offsetY = currentBounds.center.y - currentBounds.min.y + previousBounds.center.y -
+                            previousBounds.max.y;
+            currentTile.transform.position += new Vector3(-offsetX, -offsetY, 0f);
+        }
+
+        private GameObject SpawnRandomTile()
+        {
+            int randomIndex = Random.Range(0, prefabTiles.Length);
+            return Instantiate(prefabTiles[randomIndex], transform);
+        }
+
+        private float GetTileWidth(GameObject tile)
+        {
+            BoxCollider2D collider = tile.GetComponent<BoxCollider2D>();
+            if (collider != null)
+            {
+                return collider.size.x;
+            }
+
+            return 0f;
+        }
+
+        private Bounds GetTileBounds(GameObject tile)
+        {
+            BoxCollider2D collider = tile.GetComponent<BoxCollider2D>();
+            if (collider != null)
+            {
+                return collider.bounds;
+            }
+
+            return new Bounds();
+        }
+    }
+}
---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/ProceduralLevelGerenationLogic/PathGenerator.cs
@@ -0,0 +1,76 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: rimuru.dev@gmail.com
+//
+// **************************************************************** //
+
+using UnityEngine;
+
+namespace Internal.Codebase.Runtime.ProceduralLevelGerenationLogic
+{
+    public sealed class PathGenerator : MonoBehaviour
+    {
+        public Transform[] pathPrefabs;
+        public Transform pathHolderLeft;
+        public Transform pathHolderRight;
+        public float pathSpeed = 1f;
+        public float deleteXPosition = -20f;
+
+        private Transform lastPathLeft;
+        private Transform lastPathRight;
+
+        private void Start()
+        {
+            GeneratePath();
+        }
+
+        private void Update()
+        {
+            MovePath();
+        }
+
+        private void GeneratePath()
+        {
+            int randomIndex = Random.Range(0, pathPrefabs.Length);
+            Transform newPathLeft = Instantiate(pathPrefabs[randomIndex], pathHolderLeft);
+            Transform newPathRight = Instantiate(pathPrefabs[randomIndex], pathHolderRight);
+
+            // Позиционируем путь слева и справа от камеры
+            if (lastPathLeft == null)
+            {
+                newPathLeft.position = Camera.main.ViewportToWorldPoint(new Vector2(0, 0.5f));
+                newPathRight.position = Camera.main.ViewportToWorldPoint(new Vector2(1, 0.5f));
+            }
+            else
+            {
+                SpriteRenderer spriteRenderer = newPathLeft.GetComponentInChildren<SpriteRenderer>();
+                float pathWidth = spriteRenderer.bounds.size.x;
+
+                newPathLeft.position = lastPathLeft.position + new Vector3(pathWidth, 0, 0);
+                newPathRight.position = lastPathRight.position + new Vector3(pathWidth, 0, 0);
+            }
+
+            lastPathLeft = newPathLeft;
+            lastPathRight = newPathRight;
+        }
+
+        private void MovePath()
+        {
+            pathHolderLeft.position -= new Vector3(pathSpeed * Time.deltaTime, 0, 0);
+            pathHolderRight.position -= new Vector3(pathSpeed * Time.deltaTime, 0, 0);
+
+            if (lastPathLeft.position.x <= deleteXPosition)
+            {
+                DestroyLastPath();
+                GeneratePath();
+            }
+        }
+
+        private void DestroyLastPath()
+        {
+            Destroy(lastPathLeft.gameObject);
+            Destroy(lastPathRight.gameObject);
+        }
+    }
+}
