441cfe943e893145318bb53b37eb3502a6d37f73
RimuruDev <Rimuru.Dev@gmail.com>
2023-07-18 20:16:39 +0400

    Update NumberVisualizer.cs

--- NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/SpriteTextNumberCounterLogic/NumberVisualizer.cs
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/SpriteTextNumberCounterLogic/NumberVisualizer.cs
@@ -15,10 +15,16 @@ namespace Internal.Codebase.Runtime.SpriteTextNumberCounterLogic
         public Sprite[] numberSprites;
         public Image[] numberImages;
 
+        #region Visualize Text
+
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.A))
                 ShowNumber(Random.Range(0, 9999));
+            if (Input.GetKeyDown(KeyCode.W))
+                ShowNumber(0);
+            if (Input.GetKeyDown(KeyCode.R))
+                ShowNumber(145);
         }
 
         public void ShowNumber(int number)
@@ -28,18 +34,23 @@ namespace Internal.Codebase.Runtime.SpriteTextNumberCounterLogic
             if (CanNotValidNumber(number))
                 return;
 
-            var numberString = $"{number}".PadLeft(4, '0');
+            var numberString = $"{number:0000}";
 
-            for (var i = 0; i < numberString.Length; i++)
+            var length = numberString.Length;
+            for (var i = 0; i < length; i++)
             {
                 var digitChar = numberString[i];
-                var digit = int.Parse($"{digitChar}");
+                var digit = digitChar - '0';
 
                 if (digit < 0 || digit >= numberSprites.Length)
                     continue;
 
-                numberImages[i].gameObject.SetActive(true);
-                numberImages[i].sprite = numberSprites[digit];
+                var image = numberImages[i];
+                if (!image.gameObject.activeSelf)
+                {
+                    image.gameObject.SetActive(true);
+                    image.sprite = numberSprites[digit];
+                }
             }
         }
 
@@ -49,7 +60,45 @@ namespace Internal.Codebase.Runtime.SpriteTextNumberCounterLogic
         private void DisableAllImage()
         {
             foreach (var image in numberImages)
-                image.gameObject.SetActive(false);
+            {
+                if (image.gameObject.activeSelf)
+                    image.gameObject.SetActive(false);
+            }
         }
+
+        #endregion
+
+        #region Endless Auto Visualize
+
+        private int currentNumber = 0;
+
+        private void Start()
+        {
+            InvokeRepeating(nameof(UpdateNumber), 1f, .01f);
+        }
+
+        private void UpdateNumber()
+        {
+            currentNumber++;
+            var numberString = currentNumber.ToString("0000");
+
+            for (var i = 0; i < numberString.Length; i++)
+            {
+                var digitChar = numberString[i];
+                var digit = digitChar - '0';
+
+                if (digit >= 0 && digit < numberSprites.Length)
+                {
+                    numberImages[i].sprite = numberSprites[digit];
+                }
+            }
+
+            if (currentNumber >= 9999)
+            {
+                CancelInvoke(nameof(UpdateNumber));
+            }
+        }
+
+        #endregion
     }
 }
