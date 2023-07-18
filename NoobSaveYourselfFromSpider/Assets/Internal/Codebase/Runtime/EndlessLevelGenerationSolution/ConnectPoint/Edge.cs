3f4abf335f00cba11cffaf0aec3c972f0512e0d1
RimuruDev <Rimuru.Dev@gmail.com>
2023-07-18 17:09:13 +0400

    Create Edge

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/EndlessLevelGenerationSolution/ConnectPoint/Edge.cs
@@ -0,0 +1,18 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: rimuru.dev@gmail.com
+//
+// **************************************************************** //
+
+using System;
+
+namespace Internal.Codebase.Runtime.EndlessLevelGenerationSolution.ConnectPoint
+{
+    [Serializable]
+    public enum Edge
+    {
+        Left = 0,
+        Right = 1,
+    }
+}
