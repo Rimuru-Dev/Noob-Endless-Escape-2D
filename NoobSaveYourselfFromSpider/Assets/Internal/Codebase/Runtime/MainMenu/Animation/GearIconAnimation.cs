8d703a9552b828895d795f875526fb5f519ca518
RimuruDev <Rimuru.Dev@gmail.com>
2023-07-19 00:58:10 +0400

    Create animations

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/Animation/GearIconAnimation.cs
@@ -0,0 +1,62 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: rimuru.dev@gmail.com
+//
+// **************************************************************** //
+
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Internal.Codebase.Runtime.MainMenu.Animation
+{
+    [SelectionBase]
+    [DisallowMultipleComponent]
+    public sealed class GearIconAnimation : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private float animationDuration = 0.5f;
+        [SerializeField] private float rotationAngle = -180f;
+
+        private bool isAnimating;
+        private RectTransform gearIconRectTransform;
+        private Quaternion startRotation;
+
+        private void Start()
+        {
+            gearIconRectTransform = GetComponent<RectTransform>();
+            startRotation = gearIconRectTransform.localRotation;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (isAnimating)
+                return;
+
+            isAnimating = true;
+
+            if (gearIconRectTransform.localRotation == startRotation)
+            {
+                gearIconRectTransform
+                    .DOLocalRotate(new Vector3(0f, rotationAngle, 0), animationDuration, RotateMode.FastBeyond360)
+                    .SetEase(Ease.OutElastic)
+                    .OnComplete(RotateBack);
+            }
+            else
+            {
+                gearIconRectTransform
+                    .DOLocalRotate(startRotation.eulerAngles, animationDuration)
+                    .SetEase(Ease.OutQuart)
+                    .OnComplete(() => isAnimating = false);
+            }
+        }
+
+        private void RotateBack()
+        {
+            gearIconRectTransform
+                .DOLocalRotate(startRotation.eulerAngles, animationDuration)
+                .SetEase(Ease.OutQuart)
+                .OnComplete(() => isAnimating = false);
+        }
+    }
+}
---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/Animation/ImageClickAnimation.cs
@@ -0,0 +1,50 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: rimuru.dev@gmail.com
+//
+// **************************************************************** //
+
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Internal.Codebase.Runtime.MainMenu.Animation
+{
+    [SelectionBase]
+    [DisallowMultipleComponent]
+    public sealed class ImageClickAnimation : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private float scaleMultiplier = 1.2f;
+        [SerializeField] private float animationDuration = 0.2f;
+        [SerializeField] private int animationRepeat = 2;
+
+        private bool isAnimating;
+        private Vector3 originalScale;
+
+        private void Start() =>
+            originalScale = transform.localScale;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (isAnimating)
+                return;
+
+            isAnimating = true;
+
+            var targetScale = transform.localScale * scaleMultiplier;
+
+            transform
+                .DOScale(targetScale, animationDuration)
+                .SetEase(Ease.OutQuad)
+                .SetLoops(animationRepeat, LoopType.Yoyo)
+                .OnComplete(() =>
+                {
+                    transform
+                        .DOScale(originalScale, animationDuration)
+                        .SetEase(Ease.OutElastic)
+                        .OnComplete(() => isAnimating = false);
+                });
+        }
+    }
+}
