66bac5b5d8de98c6dfe5961477dd67f9ef861e3f
RimuruDev <Rimuru.Dev@gmail.com>
2023-07-18 17:10:09 +0400

    Create IEndlessLevelGenerationHandler

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/EndlessLevelGenerationSolution/Handlers/IEndlessLevelGenerationHandler.cs
@@ -0,0 +1,16 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: rimuru.dev@gmail.com
+//
+// **************************************************************** //
+
+namespace Internal.Codebase.Runtime.EndlessLevelGenerationSolution.Handlers
+{
+    public interface IEndlessLevelGenerationHandler
+    {
+        public void StartEndlessLevelGeneration();
+
+        public void StopEndlessLevelGeneration();
+    }
+}
