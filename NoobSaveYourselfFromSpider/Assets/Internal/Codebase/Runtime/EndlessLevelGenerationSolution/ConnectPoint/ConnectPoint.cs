4e1abf511a429d897dc8698b45f2e98482eb6ae4
RimuruDev <Rimuru.Dev@gmail.com>
2023-07-18 17:09:33 +0400

    Create ConnectPoint

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/EndlessLevelGenerationSolution/ConnectPoint/ConnectPoint.cs
@@ -0,0 +1,18 @@
+// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: rimuru.dev@gmail.com
+//
+// **************************************************************** //
+
+using UnityEngine;
+
+namespace Internal.Codebase.Runtime.EndlessLevelGenerationSolution.ConnectPoint
+{
+    [SelectionBase]
+    [DisallowMultipleComponent]
+    public sealed class ConnectPoint : MonoBehaviour
+    {
+        public Edge edge;
+    }
+}
