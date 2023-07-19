34319de5c9c9627123b1500c107c42890fd468d8
RimuruDev <Rimuru.Dev@gmail.com>
2023-07-19 22:17:16 +0400

    Create Storage

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/StorageData/Storage.cs
@@ -0,0 +1,67 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: rimuru.dev@gmail.com
+//
+// **************************************************************** //
+
+using System;
+using System.Collections.Generic;
+
+namespace Internal.Codebase.Runtime.StorageData
+{
+    [Serializable]
+    public sealed class Currancys
+    {
+        public int emerald;
+        public int fish;
+    }
+
+    [Serializable]
+    public sealed class UserBestDistance
+    {
+        public int bestDistance;
+    }
+
+    [Serializable]
+    public sealed class UserSkins
+    {
+        public int selectionSkinId;
+        public List<SkinData> SkinDatas;
+
+        public sealed class SkinData
+        {
+            public int ID;
+            public bool IsOpen;
+        }
+    }
+
+    [Serializable]
+    public sealed class UserBioms
+    {
+        public int selectionBiomId;
+        public List<BiomDatas> BiomData;
+
+        public sealed class BiomDatas
+        {
+            public int ID;
+            public bool IsOpen;
+        }
+    }
+
+    [Serializable]
+    public sealed class AudioSettings
+    {
+        public float volume;
+    }
+
+    [Serializable]
+    public sealed class Storage
+    {
+        public Currancys currancys;
+        public UserBestDistance userBestDistance;
+        public UserSkins userSkins;
+        public UserBioms userBioms;
+        public AudioSettings audioSettings;
+    }
+}
