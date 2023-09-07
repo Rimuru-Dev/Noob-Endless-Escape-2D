8b9cbfd49c2166e76dd5fdd9b14d6b8a9a67e019
Rimuru <Rimuru.Dev@gmail.com>
2023-09-07 17:12:25 +0400

    Create QuickAccessBuyCurrencyView.cs

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/New/View/QuickAccessBuyCurrencyView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Internal.Codebase.Runtime.MainMenu.New.View
+{
+    [DisallowMultipleComponent]
+    public sealed class QuickAccessBuyCurrencyView : MonoBehaviour
+    {
+        [field: SerializeField] public Button BuyFishButton { get; private set; }
+        [field: SerializeField] public Button BuyEmeraldButton { get; private set; }
+    }
+}
