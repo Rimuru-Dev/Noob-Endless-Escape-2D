3d3f731d2380007aab306f6dec4336ba7987c3c2
Rimuru <Rimuru.Dev@gmail.com>
2023-09-08 00:57:18 +0400

    Update CurrancyUIView.cs

--- NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/CurrancyUIView.cs
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/CurrancyUIView.cs
@@ -1,55 +1,37 @@
 ﻿using System;
-using Internal.Codebase.Runtime.MainMenu.Animation;
+using Internal.Codebase.Infrastructure.Services.CloudSave;
 using Internal.Codebase.Runtime.SpriteTextNumberCounterLogic;
 using Internal.Codebase.Runtime.StorageData;
 using UnityEngine;
-using YG;
 
 namespace Internal.Codebase.Runtime.MainMenu
 {
-    [Serializable]
-    public enum CurrancyTypeID
-    {
-        Emerald = 0,
-        Fish = 1,
-    }
-
-    public sealed class CurrancyUIView : MonoBehaviour, IFuckingSaveLoad
+    [DisallowMultipleComponent]
+    public sealed class CurrancyUIView : MonoBehaviour
     {
         [field: SerializeField] public CurrancyTypeID CurrancyTypeID { get; set; }
         [field: SerializeField] public NumberVisualizer NumberVisualizer { get; private set; }
 
         private Storage storage;
+        private IYandexSaveService yandexSaveService;
 
-        private void Awake()
+        public void Constructor(IYandexSaveService saveService) =>
+            yandexSaveService = saveService;
+
+        private void OnDestroy() =>
+            UnsubscribeNumberVisualizer();
+
+        public void Prepare()
         {
-            if (YandexGame.SDKEnabled)
-                Load();
+            storage = yandexSaveService.Load();
 
-            YandexGame.GetDataEvent += Load;
+            SubscribeNumberVisualizer();
+
+            storage.Refresh();
         }
 
-        public void Save()
+        private void SubscribeNumberVisualizer()
         {
-            YandexGame.savesData.storage.fishCurrancy = storage.fishCurrancy;
-            YandexGame.savesData.storage.emeraldCurrancy = storage.emeraldCurrancy;
-        }
-
-        public void Load()
-        {
-            if (YandexGame.savesData.storage.emeraldCurrancy == null ||
-                YandexGame.savesData.storage.fishCurrancy == null)
-            {
-                YandexGame.savesData.storage.emeraldCurrancy = new EmeraldCurrancy();
-                YandexGame.savesData.storage.fishCurrancy = new FishCurrancy();
-                YandexGame.savesData.storage.emeraldCurrancy.emeralds = 0;
-                YandexGame.savesData.storage.fishCurrancy.fishs = 0;
-                storage = YandexGame.savesData.storage;
-            }
-            else
-                storage = YandexGame.savesData.storage;
-
-
             switch (CurrancyTypeID)
             {
                 case CurrancyTypeID.Emerald:
@@ -61,28 +43,9 @@ namespace Internal.Codebase.Runtime.MainMenu
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-
-            storage.Refresh();
         }
 
-        public void Initialize(Storage storage)
-        {
-            // this.storage = storage;
-            //
-            // switch (CurrancyTypeID)
-            // {
-            //     case CurrancyTypeID.Emerald:
-            //         storage.OnEmeraldCurrancyChanged += NumberVisualizer.ShowNumber;
-            //         break;
-            //     case CurrancyTypeID.Fish:
-            //         storage.OnFishCurrancyChanged += NumberVisualizer.ShowNumber;
-            //         break;
-            //     default:
-            //         throw new ArgumentOutOfRangeException();
-            // }
-        }
-
-        private void OnDestroy()
+        private void UnsubscribeNumberVisualizer()
         {
             switch (CurrancyTypeID)
             {
@@ -95,8 +58,6 @@ namespace Internal.Codebase.Runtime.MainMenu
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-
-            YandexGame.GetDataEvent -= Load;
         }
     }
 }
