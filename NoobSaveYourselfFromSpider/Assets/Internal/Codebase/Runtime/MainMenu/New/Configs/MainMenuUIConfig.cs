65a9463a5b36d26c407bddddfae214299c2bf9e3
Rimuru <Rimuru.Dev@gmail.com>
2023-09-07 17:12:32 +0400

    Create MainMenuUIConfig.cs

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/New/Configs/MainMenuUIConfig.cs
@@ -0,0 +1,27 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Project: "Murders Drones Endless Way" 
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+using Internal.Codebase.Runtime.MainMenu.New.UI;
+using Internal.Codebase.Runtime.MainMenu.New.View;
+using UnityEngine;
+
+namespace Internal.Codebase.Runtime.MainMenu.New.Configs
+{
+    [CreateAssetMenu(fileName = nameof(MainMenuUIConfig), menuName = "Static Data/Main Menu Scene/UI Config",
+        order = 0)]
+    public sealed class MainMenuUIConfig : ScriptableObject
+    {
+        [field: SerializeField] public UIRoot UIRoot { get; private set; }
+        [field: SerializeField] public BackgroundCanvasView BackgroundCanvasView { get; private set; }
+        [field: SerializeField] public MainMenuCanvasView MainMenuCanvasView { get; private set; }
+    }
+}
