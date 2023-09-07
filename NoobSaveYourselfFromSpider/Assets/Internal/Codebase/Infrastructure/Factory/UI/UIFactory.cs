491efe5309a1cd9a09c3408e7605b537c5678f2b
Rimuru <Rimuru.Dev@gmail.com>
2023-09-08 00:58:02 +0400

    Update UIFactory.cs

--- NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Infrastructure/Factory/UI/UIFactory.cs
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Infrastructure/Factory/UI/UIFactory.cs
@@ -5,84 +5,20 @@
 //
 // **************************************************************** //
 
-using System;
 using Internal.Codebase.Infrastructure.AssetManagement;
-using Internal.Codebase.Infrastructure.Services;
-using Internal.Codebase.Infrastructure.Services.ActionUpdater;
 using Internal.Codebase.Infrastructure.Services.StaticData;
-using Internal.Codebase.Infrastructure.Services.Storage;
 using Internal.Codebase.Runtime.Curtain;
-using Internal.Codebase.Runtime.MainMenu;
-using Internal.Codebase.Runtime.MainMenu.New.Configs;
 using Internal.Codebase.Runtime.MainMenu.New.UI;
-using UnityEngine;
 using Zenject;
 
 namespace Internal.Codebase.Infrastructure.Factory.UI
 {
     public sealed class UIFactory : IUIFactory
     {
-        private readonly IActionUpdaterService actionUpdaterService;
         private readonly IAssetProvider assetProvider;
         private readonly IStaticDataService staticData;
-        private readonly IStorageService storageService;
-
-        private Transform mainMenuRoot;
-
-        [Inject]
-        public UIFactory(
-            IActionUpdaterService actionUpdaterService,
-            IAssetProvider assetProvider,
-            IStaticDataService staticData,
-            IStorageService storageService)
-        {
-            this.actionUpdaterService = actionUpdaterService;
-            this.assetProvider = assetProvider;
-            this.staticData = staticData;
-            this.storageService = storageService;
-        }
-
-        public Transform CreateMainMenuRoot()
-        {
-            var config = staticData.ForMainMenu();
-
-            mainMenuRoot = assetProvider.Instantiate(config.UIRoot.gameObject).transform;
-
-            return mainMenuRoot;
-        }
-
-        public CurtainView CreateCurtain()
-        {
-            var config = staticData.ForCurtain();
-            var instance = assetProvider.Instantiate(config.CurtainView.gameObject);
-            var view = instance.GetComponent<CurtainView>();
-
-            view.Constructor(config.AnimationDuration);
-
-            return view;
-        }
-
-        public GameObject CreateDynamicCanvas()
-        {
-            var config = staticData.ForMainMenu();
-
-            var instance = assetProvider.Instantiate(config.DynamicCanvas.gameObject, mainMenuRoot);
-
-            return instance;
-        }
-
-        public MainMenuCanvasView CreateMainMenuCanvas()
-        {
-            var config = staticData.ForMainMenu();
-
-            var view = assetProvider
-                .Instantiate(config.MainMenuCanvas.gameObject, mainMenuRoot)
-                .GetComponent<MainMenuCanvasView>();
-
-            return view;
-        }
-
         private UIRoot mainMenuUIRoot;
+
         public UIRoot MainMenuUIRoot
         {
             get
@@ -97,6 +33,24 @@ namespace Internal.Codebase.Infrastructure.Factory.UI
                 if (mainMenuUIRoot == null && value != null)
                     mainMenuUIRoot = value;
             }
+        }
+
+        [Inject]
+        public UIFactory(IAssetProvider assetProvider, IStaticDataService staticData)
+        {
+            this.assetProvider = assetProvider;
+            this.staticData = staticData;
+        }
+
+        public CurtainView CreateCurtain()
+        {
+            var config = staticData.ForCurtain();
+            var instance = assetProvider.Instantiate(config.CurtainView.gameObject);
+            var view = instance.GetComponent<CurtainView>();
+
+            view.Constructor(config.AnimationDuration);
+
+            return view;
         }
 
         public IUIFactory CreateMainMenuUIRoot()
