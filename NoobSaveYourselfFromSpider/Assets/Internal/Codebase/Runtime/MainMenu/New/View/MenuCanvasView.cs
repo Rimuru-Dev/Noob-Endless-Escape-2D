c07f46d50d661b1bfd5e9203a33a8ee1324ed4db
Rimuru <Rimuru.Dev@gmail.com>
2023-09-07 17:12:06 +0400

    Create MenuCanvasView.cs

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/New/View/MenuCanvasView.cs
@@ -0,0 +1,20 @@
+﻿using Internal.Codebase.Runtime.BiomeShop;
+using UnityEngine;
+
+namespace Internal.Codebase.Runtime.MainMenu.New.View
+{
+    [SelectionBase]
+    [DisallowMultipleComponent]
+    public sealed class MenuCanvasView : MonoBehaviour
+    {
+        [field: SerializeField] public CurrancyUIView CurrancFishView { get; private set; }
+        [field: SerializeField] public CurrancyUIView CurrancEmeraldView { get; private set; }
+
+        [field: SerializeField] public SettingsView SettingsView { get; private set; }
+        [field: SerializeField] public CurrencyShopView CurrencyShopView { get; private set; }
+        [field: SerializeField] public BestDistanceUIView BestDistanceView { get; private set; }
+        [field: SerializeField] public QuickAccessBuyCurrencyView QuickAccessBuyCurrencyView { get; private set; }
+        [field: SerializeField] public CharacterSwitcherView CharacterSwitcherView { get; private set; }
+        [field: SerializeField] public BiomeShopView BiomeShopView { get; private set; }
+    }
+}
