8499a1642b3987171ada8f7d6cedfdc9d7f459e8
Rimuru <Rimuru.Dev@gmail.com>
2023-09-08 00:57:11 +0400

    Update CharacterSwitcher.cs

--- NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/Animation/CharacterSwitcher.cs
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/Animation/CharacterSwitcher.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using Internal.Codebase.Infrastructure.Services.CloudSave;
 using Internal.Codebase.Runtime.SpriteTextNumberCounterLogic;
 using Internal.Codebase.Runtime.StorageData;
 using YG;
@@ -37,93 +38,58 @@ namespace Internal.Codebase.Runtime.MainMenu.Animation
         public CurrancyTypeID currancyTypeID;
     }
 
-    public interface IFuckingSaveLoad
-    {
-        public void Save();
-        public void Load();
-    }
 
-    public sealed class CharacterSwitcher : MonoBehaviour, IFuckingSaveLoad
+    // TODO: Remove MonoBehaviour
+    public sealed class CharacterSwitcher : MonoBehaviour
     {
         public Image characterImage;
         public List<SkinShopData> skins;
         public Button buyButton;
-
         public GameObject selectSkin;
         public Image cyrrancy;
         public NumberVisualizer numberVisualizer;
-
         public List<CurrancyIcons> currancyIcons;
 
         private RectTransform characterTransform;
         private int currentIndex;
         private bool isSwitching;
-
         private int currentActualSkinId;
         private int selectionSkinID;
+        private Storage storage;
+        private IYandexSaveService saveService;
 
-        //     private Storage storage;
-        //    private IPersistenProgressService persistenProgressService;
+        public void Constructor(IYandexSaveService yandexSaveService) =>
+            saveService = yandexSaveService;
 
-        [System.Diagnostics.Conditional("DEBUG")]
-        private void OnValidate()
+        public void Prepape()
         {
-            int id = -1;
-            foreach (var skin in skins)
-            {
-                id++;
-                skin.id = id;
-            }
-        }
+            storage = saveService.Load();
 
-        private void Awake()
-        {
-            if (YandexGame.SDKEnabled)
-                Load();
-
-            YandexGame.GetDataEvent += Load;
-        }
-
-        public void Save()
-        {
-            // if (storage != null)
-            // {
-            //     if (YandexGame.savesData.storage == null)
-            //     {
-            //         YandexGame.savesData.storage = new Storage();
-            //         YandexGame.savesData.storage = storage;
-            //         Debug.Log("FATAL ERROR SKIN SHOP");
-            //     }
-            //     else
-            //         YandexGame.savesData.storage = storage;
-            // }
-
-            // YandexGame.savesData.storage.userSkins = storage.userSkins;
-            YandexGame.savesData.storage.userSkins = storage.userSkins;
-            YandexGame.savesData.storage.fishCurrancy = storage.fishCurrancy;
-            YandexGame.savesData.storage.emeraldCurrancy = storage.emeraldCurrancy;
-        }
-
-        public Storage storage;
-
-        public void Load()
-        {
-            if (YandexGame.savesData.storage.userSkins == null)
+            if (storage.userSkins == null)
                 LoadDefault();
             else
                 LoadUserdata();
 
-            currentActualSkinId = storage.userSkins.selectionSkinId;
-            characterTransform = characterImage.GetComponent<RectTransform>();
-            characterTransform.localScale = Vector3.zero;
-            AnimateCharacter(true, currentActualSkinId);
-            UpdateUI(currentActualSkinId);
-            buyButton.onClick.AddListener(Buy);
+            Refrash();
+
+            Save();
+
+            void Refrash()
+            {
+                currentActualSkinId = storage.userSkins.selectionSkinId;
+                characterTransform = characterImage.GetComponent<RectTransform>();
+                characterTransform.localScale = Vector3.zero;
+                AnimateCharacter(true, currentActualSkinId);
+                UpdateUI(currentActualSkinId);
+                buyButton.onClick.AddListener(Buy);
+            }
 
             void LoadDefault()
             {
-                YandexGame.savesData.storage.userSkins = new UserSkins();
-                YandexGame.savesData.storage.userSkins.SkinDatas = new List<SkinData>();
+                storage.userSkins = new UserSkins
+                {
+                    SkinDatas = new List<SkinData>()
+                };
 
                 foreach (var skin in skins)
                 {
@@ -131,74 +97,28 @@ namespace Internal.Codebase.Runtime.MainMenu.Animation
                     skinData.ID = skin.id;
                     skinData.IsOpen = skin.isOpen;
 
-                    YandexGame.savesData.storage.userSkins.SkinDatas.Add(skinData);
+                    storage.userSkins.SkinDatas.Add(skinData);
                 }
 
-                YandexGame.savesData.storage.userSkins.SkinDatas[0].IsOpen = true;
-                YandexGame.savesData.storage.userSkins.selectionSkinId =
-                    YandexGame.savesData.storage.userSkins.SkinDatas[0].ID;
-
-                storage = YandexGame.savesData.storage;
+                storage.userSkins.SkinDatas[0].IsOpen = true;
+                storage.userSkins.selectionSkinId = storage.userSkins.SkinDatas[0].ID;
             }
 
             void LoadUserdata()
             {
-                storage = YandexGame.savesData.storage;
-
                 foreach (var userSkin in storage.userSkins.SkinDatas)
                 {
                     foreach (var skin in skins.Where(skin => userSkin.ID == skin.id))
-                    {
                         skin.isOpen = userSkin.IsOpen;
-                    }
                 }
             }
         }
 
-        // public void Initialize(Storage storage, IPersistenProgressService persistenProgressService)
-        // {
-        //    // this.storage = storage;
-        //   //  this.persistenProgressService = persistenProgressService;
-        //
-        //     if (storage.userSkins.SkinDatas == null || storage.userSkins.SkinDatas.Count <= 0)
-        //     {
-        //         storage.userSkins.SkinDatas = new List<SkinData>();
-        //
-        //         foreach (var skin in skins)
-        //         {
-        //             var skinData = new SkinData();
-        //             skinData.ID = skin.id;
-        //             skinData.IsOpen = skin.isOpen;
-        //             storage.userSkins.SkinDatas.Add(skinData);
-        //         }
-        //
-        //         storage.userSkins.SkinDatas[0].IsOpen = true;
-        //         storage.userSkins.selectionSkinId = storage.userSkins.SkinDatas[0].ID;
-        //
-        //         if (YandexGame.SDKEnabled)
-        //             persistenProgressService.Save(this.storage);
-        //     }
-        //     else
-        //     {
-        //         foreach (var userSkin in storage.userSkins.SkinDatas)
-        //         {
-        //             foreach (var skin in skins.Where(skin => userSkin.ID == skin.id))
-        //             {
-        //                 skin.isOpen = userSkin.IsOpen;
-        //             }
-        //         }
-        //     }
-        //
-        //     currentActualSkinId = storage.userSkins.selectionSkinId;
-        //
-        //     characterTransform = characterImage.GetComponent<RectTransform>();
-        //     characterTransform.localScale = Vector3.zero;
-        //
-        //     AnimateCharacter(true, currentActualSkinId);
-        //     UpdateUI(currentActualSkinId);
-        //
-        //     buyButton.onClick.AddListener(Buy);
-        // }
+        private void OnDestroy() =>
+            buyButton.onClick.RemoveListener(Buy);
+
+        private void Save() =>
+            saveService.Save(storage);
 
         private void Buy()
         {
@@ -225,7 +145,6 @@ namespace Internal.Codebase.Runtime.MainMenu.Animation
                         cyrrancy.gameObject.SetActive(false);
 
                         Save();
-                        // persistenProgressService.Save(storage);
                     }
                 }
                     break;
@@ -245,7 +164,6 @@ namespace Internal.Codebase.Runtime.MainMenu.Animation
                         cyrrancy.gameObject.SetActive(false);
 
                         Save();
-                        // persistenProgressService.Save(storage);
                     }
                 }
                     break;
@@ -334,12 +252,6 @@ namespace Internal.Codebase.Runtime.MainMenu.Animation
                 numberVisualizer.gameObject.SetActive(false);
                 cyrrancy.gameObject.SetActive(false);
             }
-        }
-
-        private void OnDestroy()
-        {
-            YandexGame.GetDataEvent -= Load;
-            buyButton.onClick.RemoveListener(Buy);
         }
     }
 }
