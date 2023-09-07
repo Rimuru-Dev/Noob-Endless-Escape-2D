adbedb4dd4dc58ad077d46ff3b31005e981b4596
Rimuru <Rimuru.Dev@gmail.com>
2023-09-07 17:11:56 +0400

    Create CharacterSwitcherView

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/New/View/CharacterSwitcherView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Internal.Codebase.Runtime.MainMenu.Animation;
+using Internal.Codebase.Runtime.SpriteTextNumberCounterLogic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Internal.Codebase.Runtime.MainMenu.New.View
+{
+    [DisallowMultipleComponent]
+    public sealed class CharacterSwitcherView : MonoBehaviour
+    {
+        public Image characterImage;
+        public List<SkinShopData> skins;
+        public Button buyButton;
+
+        public GameObject selectSkin;
+        public Image cyrrancy;
+        public NumberVisualizer numberVisualizer;
+
+        public List<CurrancyIcons> currancyIcons;
+    }
+}
