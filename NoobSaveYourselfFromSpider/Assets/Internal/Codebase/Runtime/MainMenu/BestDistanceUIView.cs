5f722b4f866f41c67323825014c8f5a3c8f007f4
Rimuru <Rimuru.Dev@gmail.com>
2023-09-08 00:57:15 +0400

    Update BestDistanceUIView.cs

--- NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/BestDistanceUIView.cs
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/BestDistanceUIView.cs
@@ -5,52 +5,32 @@
 //
 // **************************************************************** //
 
-using System;
-using Internal.Codebase.Runtime.MainMenu.Animation;
+using Internal.Codebase.Infrastructure.Services.CloudSave;
 using Internal.Codebase.Runtime.SpriteTextNumberCounterLogic;
 using Internal.Codebase.Runtime.StorageData;
 using UnityEngine;
-using YG;
 
 namespace Internal.Codebase.Runtime.MainMenu
 {
-    public sealed class BestDistanceUIView : MonoBehaviour, IFuckingSaveLoad
+    [DisallowMultipleComponent]
+    public sealed class BestDistanceUIView : MonoBehaviour
     {
         [field: SerializeField] public NumberVisualizer NumberVisualizer { get; private set; }
 
         private Storage storage;
+        private IYandexSaveService saveService;
 
-        private void Awake()
+        public void Constuctor(IYandexSaveService yandexSaveService) =>
+            saveService = yandexSaveService;
+
+        public void Prepare()
         {
-            if (YandexGame.SDKEnabled)
-                Load();
-
-            YandexGame.GetDataEvent += Load;
-        }
-
-        public void Save()
-        {
-            YandexGame.savesData.storage.BestDistance = storage.BestDistance;
-        }
-
-        public void Load()
-        {
-            storage = YandexGame.savesData.storage;
+            storage = saveService.Load();
             storage.OnBestDistanceChanged += NumberVisualizer.ShowNumber;
             storage.Refresh();
         }
 
-        public void Initialize(Storage storage)
-        {
-            //     this.storage = storage;
-
-            //   storage.OnBestDistanceChanged += NumberVisualizer.ShowNumber;
-        }
-
-        private void OnDestroy()
-        {
-            YandexGame.GetDataEvent -= Load;
+        private void OnDestroy() =>
             storage.OnBestDistanceChanged -= NumberVisualizer.ShowNumber;
-        }
     }
 }
