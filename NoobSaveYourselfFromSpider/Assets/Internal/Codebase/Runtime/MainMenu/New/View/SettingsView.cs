faf33c99bfb922bc8b230cb80c9b8427e39cca2a
Rimuru <Rimuru.Dev@gmail.com>
2023-09-07 17:12:40 +0400

    Create SettingsView.cs

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/MainMenu/New/View/SettingsView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Internal.Codebase.Runtime.MainMenu.New.View
+{
+    public sealed class SettingsView : MonoBehaviour
+    {
+        // InteractionWindow
+        [field: SerializeField] public GameObject Panel { get; private set; }
+        [field: SerializeField] public Button OpenPanel { get; private set; }
+        [field: SerializeField] public Button ClosePanel { get; private set; }
+
+        // Audio Settings
+        [field: SerializeField] public Slider AudioSlider { get; private set; }
+    }
+}
