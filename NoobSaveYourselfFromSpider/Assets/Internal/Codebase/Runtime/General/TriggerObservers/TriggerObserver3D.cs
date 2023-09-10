ab16934908662f45bf137846cc9ca1c2c0d554e4
Rimuru <Rimuru.Dev@gmail.com>
2023-09-10 11:39:48 +0400

    Create TriggerObserver3D.cs

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Runtime/General/TriggerObservers/TriggerObserver3D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Internal.Codebase.Runtime.General.TriggerObservers
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Collider))]
+    public sealed class TriggerObserver3D : MonoBehaviour
+    {
+        public event Action<Collider> OnCollisionEnter;
+        public event Action<Collider> OnCollisionStay;
+        public event Action<Collider> OnCollisionExit;
+
+        private void OnTriggerEnter(Collider other) =>
+            OnCollisionEnter?.Invoke(other);
+
+        private void OnTriggerStay(Collider other) =>
+            OnCollisionStay?.Invoke(other);
+
+        private void OnTriggerExit(Collider other) =>
+            OnCollisionExit?.Invoke(other);
+    }
+}
