ba9c4978c1240c44f9a26230e431506564b787bc
Rimuru <Rimuru.Dev@gmail.com>
2023-09-10 11:39:03 +0400

    Update GameOverState.cs

--- NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Infrastructure/GeneralGameStateMachine/States/GameOverState.cs
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Infrastructure/GeneralGameStateMachine/States/GameOverState.cs
@@ -10,7 +10,27 @@ using Internal.Codebase.Infrastructure.GeneralGameStateMachine.StateMachine;
 
 namespace Internal.Codebase.Infrastructure.GeneralGameStateMachine.States
 {
+    // В этом классе обработать вообще все что связано с GameOver
+    // Очищать все ресы после перехода в MainMenu
     public sealed class GameOverState : IStateNext
+    {
+        public void Init(GameStateMachine stateMachine)
+        {
+        }
+
+        public void Exit()
+        {
+        }
+
+        public void Enter()
+        {
+        }
+    }
+    
+    /// <summary>
+    /// // TODO: Пересобрать мир но сохранить очки.
+    /// </summary>
+    public sealed class RebirdthState : IStateNext
     {
         public void Init(GameStateMachine stateMachine)
         {
