44268200c64cd6e614a6dca689a0f7acabc11c51
Rimuru <Rimuru.Dev@gmail.com>
2023-09-10 11:38:20 +0400

    Create CleanupService.cs

---
+++ NoobSaveYourselfFromSpider/Assets/Internal/Codebase/Infrastructure/Services/Cleanup/ICleanupService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Internal.Codebase.Infrastructure.Services.Cleanup
+{
+    public interface ICleanupService : IDisposable
+    {
+        public void Cleanup();
+        public void RegisterCleanupAction(Action cleanupAction);
+        public void UnregisterCleanupAction(Action cleanupAction);
+    }
+}
